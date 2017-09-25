d87e5447dc7b04498fc3e902bf6eabfe3e750917
Thanawat Hanthong <peet6015@gmail.com>
2017-09-25 15:32:30 +0700

    add button to toolbar

--- app/src/main/java/biz/mwork/testsign/DrawActivity.kt
+++ app/src/main/java/biz/mwork/testsign/DrawActivity.kt
@@ -2,11 +2,17 @@ package biz.mwork.testsign
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_draw.*
 
 class DrawActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_draw)
+
+        toolBar.navigationIcon = getDrawable(R.drawable.ic_close_black_24dp)
+        toolBar.setNavigationOnClickListener {
+            finish()
+        }
     }
 }
