33ad3446c9bf2c342141aab3d740817fc150d2fa
Thanawat Hanthong <peet6015@gmail.com>
2017-09-25 14:45:53 +0700

    add title to toolbar

--- app/src/main/java/biz/mwork/testsign/MainActivity.kt
+++ app/src/main/java/biz/mwork/testsign/MainActivity.kt
@@ -15,7 +15,6 @@ class MainActivity : AppCompatActivity() {
         supportFragmentManager.beginTransaction()
                 .replace(R.id.Fragment, cFragment, "SignFragment").commit()*/
 
-        toolBar.title = "Main"
         button.setOnClickListener {
             startActivity<DrawActivity>()
         }
